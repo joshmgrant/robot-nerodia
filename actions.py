221db36a92a05ecda198a9bb0f8137becf765ab1
joshgrant <joshua.m.grant@gmail.com>
2019-05-08 00:17:54 -0400

    finally figured it out

--- actions.py
+++ actions.py
@@ -1,18 +1,35 @@
 from nerodia.browser import Browser
+from robot.api.deco import keyword
 
 BASE_TEST_URL = 'http:/localhost:9292/'
 
-def open_browser(name='firefox'):
-    browser = Browser(browser=name)
-    return browser
+class Login():
 
-def go_to_login(browser):
-    browser.goto(BASE_TEST_URL + "login")
+    def __init__(self):
+        self.browser = {}
 
-def login_as(browser, username, password):
-    browser.input(id='username').send_keys(username)
-    browser.input(id='password').send_keys(password)
-    browser.button(class_name='radius').click()
+    @keyword
+    def open_browser(self, name='firefox'):
+        self.browser = Browser(browser=name)
 
-def close_browser(browser):
-    browser.quit()
+    @keyword
+    def go_to_login(self):
+        self.browser.goto(BASE_TEST_URL + "login")
+
+    @keyword
+    def login_as(self, username, password):
+        self.browser.input(id='username').send_keys(username)
+        self.browser.input(id='password').send_keys(password)
+        self.browser.button(class_name='radius').click()
+
+    @keyword
+    def is_error_message_visible(self):
+        assert self.browser.element(class_name='error').present
+
+    @keyword
+    def is_login_successful(self):
+        assert self.browser.element(class_name='success').present
+
+    @keyword
+    def close_browser(self):
+        self.browser.quit()
--- login_test.robot
+++ login_test.robot
@@ -1,22 +1,20 @@
 *** Settings ***
 Library  actions.py
-Library  checks.py
+Library  actions.Login
 
+Test Setup    Open Browser
+Test Teardown    Close Browser
 
 *** Test Cases ***
 
 Valid Login with Standard User
 
-        ${browser}    Open Browser
-	Go To Login    ${browser}
-	Login As    ${browser}    tomsmith    SuperSecretPassword!
-	Should Be Displayed     ${browser}    .success
-        Close Browser    ${browser}
+	Go To Login
+	Login As    tomsmith    SuperSecretPassword!
+	Is Login Successful
 
 Invalid Login with Unknown User
 
-        ${browser}    Open Browser
-	Go To Login    ${browser}
-	Login As    ${browser}    bad    also_bad
-	Should Be Displayed     ${browser}    .error
-        Close Browser    ${browser}
+	Go To Login
+	Login As    bad    also_bad
+	Is Error Message Visible
