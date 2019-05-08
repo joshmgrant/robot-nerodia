4f9e93d9462da4e7adae2162094492ab58e2b1a8
joshgrant <joshua.m.grant@gmail.com>
2019-05-07 21:16:44 -0400

    back on the right track

--- actions.py
+++ actions.py
@@ -1,25 +1,18 @@
 from nerodia.browser import Browser
-from robot.api.deco import keyword
 
-BASE_TEST_URL = 'http://localhost:9292/'
+BASE_TEST_URL = 'http:/localhost:9292/'
 
-class LoginPage():
+def open_browser(name='firefox'):
+    browser = Browser(browser=name)
+    return browser
 
-    @keyword
-    def open_browser(self, name='firefox'):
-        self.browser = Browser(browser=name)
-        return self.browser
+def go_to_login(browser):
+    browser.goto(BASE_TEST_URL + "login")
 
-    @keyword
-    def go_to_login(self):
-        self.browser.goto(BASE_TEST_URL + "login")
+def login_as(browser, username, password):
+    browser.input(id='username').send_keys(username)
+    browser.input(id='password').send_keys(password)
+    browser.button(class_name='radius').click()
 
-    @keyword
-    def login_as(self, username, password):
-        self.browser.input(id='username').send_keys(username)
-        self.browser.input(id='password').send_keys(password)
-        self.browser.button(class_name='radius').click()
-
-    @keyword
-    def close_browser(self):
-        self.browser.quit()
+def close_browser(browser):
+    browser.quit()
--- login_test.robot
+++ login_test.robot
@@ -2,20 +2,21 @@
 Library  actions.py
 Library  checks.py
 
+
 *** Test Cases ***
 
 Valid Login with Standard User
 
-        Open Browser
-	Go To Login 
-	Login As    tomsmith    SuperSecretPassword!
-	Should Be Displayed    .success
-        Close Browser
+        ${browser}    Open Browser
+	Go To Login    ${browser}
+	Login As    ${browser}    tomsmith    SuperSecretPassword!
+	Should Be Displayed     ${browser}    .success
+        Close Browser    ${browser}
 
-# Invalid Login with Unknown User
+Invalid Login with Unknown User
 
-#         ${browser}    Open Browser   chrome
-# 	Go To Login    ${browser}
-# 	Login As    ${browser}    bad    also_bad
-# 	Should Be Displayed     ${browser}    .error
-#         Close Browser    ${browser}
+        ${browser}    Open Browser
+	Go To Login    ${browser}
+	Login As    ${browser}    bad    also_bad
+	Should Be Displayed     ${browser}    .error
+        Close Browser    ${browser}
